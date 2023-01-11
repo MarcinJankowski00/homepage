db0c517193608ae7c232fc756f0381dd7b3cf820
Marcin Jankowski <marcin5771@gmail.com>
2023-01-11 22:19:40 +0100

    Refactored script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 {
-    const modeswitcher = document.querySelector(".js-navigation__modeswitcher");
-
-    modeswitcher.addEventListener("click", () => {
+    const onButtonClick = () => {
         const body = document.querySelector(".js-body");
         const themeName = document.querySelector(".js-themeName");
         body.classList.toggle("body--dark");
 
         themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
-    });
+    };
+    const init = () => {
+        const modeswitcher = document.querySelector(".js-navigation__modeswitcher");
+        modeswitcher.addEventListener("click", onButtonClick)
+    } 
+
+    init()
 }
